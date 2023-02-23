2bc69d3aa06f332350392518899acd60d41bc7c2
MISUNDERSTOOD <2djohar@gmail.com>
2023-02-23 20:17:53 +0200

    fixed column rename procedure

--- pages/04_Returns_-_Reviews_classifier.py
+++ pages/04_Returns_-_Reviews_classifier.py
@@ -111,10 +111,11 @@ if st.session_state['login']:
     if 'file' in st.session_state:
         columns = (st.session_state['file']).columns.tolist()
         text_column = block1.selectbox('Select a column with comments', columns)
-        st.write(text_column)
         if st.button('Run classifier'):
-            st.session_state['file'] = st.session_state['file'].rename(columns = {text_column:'original_text'})
-            st.session_state['file'] = st.session_state['file'].dropna(subset = text_column, axis = 1)
-            st.dataframe((st.session_state['file']).head())
-            lr,cv = restore_from_file()
-            predicting(lr,cv,source = 'file')
+            st.write(text_column)
+
+            # st.session_state['file'] = st.session_state['file'].rename(columns = {text_column:'original_text'})
+            # st.session_state['file'] = st.session_state['file'].dropna(subset = text_column, axis = 1)
+            # st.dataframe((st.session_state['file']).head())
+            # lr,cv = restore_from_file()
+            # predicting(lr,cv,source = 'file')
