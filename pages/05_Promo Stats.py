711c9959dd3f2d1ca9af3826cd03692e6aea9bd2
MISUNDERSTOOD <2djohar@gmail.com>
2023-03-03 13:30:55 +0200

    wrapped promo page into main function

--- pages/05_Promo Stats.py
+++ pages/05_Promo Stats.py
@@ -12,224 +12,224 @@ st.set_page_config(page_title = 'M Tools App', page_icon = 'media/logo.ico',layo
 
 st.session_state['login'], st.session_state['name'] = login.login()
 
-bad_chars = [
-    '%off any%',
-    '%off for non-prime customers%',
-    'AB DEX Incentives%',
-    'ABMarketing:%',
-    'ABMktg%',
-    'Acquisition Promotion for Prime Day',
-    'ADEX%',
-    'AFD Base Promo%',
-    'Discover PAGE%',
-    'EFF Promotion%',
-    'Exports Free Shipping',
-    'FAP offer%',
-    'First App purchase%',
-    'Free Delivery On First Order%',
-    'Free Same Day%',
-    'Free Sub SameDay%',
-    'PAGE-Venmo%',
-    'PD21 BXGY',
-    'PROD_%',
-    'Project Diamond%',
-    '%AB Incentives%',
-    'SameDay US%',
-    'Promo Propensity%',
-    'VPC-%',
-    'TDU Promotion%',
-    'US Core Free Shipping%',
-    'USAA PAGE%',
-    'User Activation Promotion%',
-    'Venmo-Maple%'
-    ]
+if st.session_state['login']:
 
-ld_promos = [
-    'DCMS%', 'Mellanni%'
-    ]
+    bad_chars = [
+        '%off any%',
+        '%off for non-prime customers%',
+        'AB DEX Incentives%',
+        'ABMarketing:%',
+        'ABMktg%',
+        'Acquisition Promotion for Prime Day',
+        'ADEX%',
+        'AFD Base Promo%',
+        'Discover PAGE%',
+        'EFF Promotion%',
+        'Exports Free Shipping',
+        'FAP offer%',
+        'First App purchase%',
+        'Free Delivery On First Order%',
+        'Free Same Day%',
+        'Free Sub SameDay%',
+        'PAGE-Venmo%',
+        'PD21 BXGY',
+        'PROD_%',
+        'Project Diamond%',
+        '%AB Incentives%',
+        'SameDay US%',
+        'Promo Propensity%',
+        'VPC-%',
+        'TDU Promotion%',
+        'US Core Free Shipping%',
+        'USAA PAGE%',
+        'User Activation Promotion%',
+        'Venmo-Maple%'
+        ]
 
-negative_filter = bad_chars+ld_promos
-negative_filter = [x.replace('%','') for x in negative_filter]
+    ld_promos = [
+        'DCMS%', 'Mellanni%'
+        ]
 
-@st.cache_data(show_spinner=False)
-def read_promos(
-    report = 'auxillary_development',
-    table = 'promotions',
-    column = 'description',
-    code_list = None,
-    start = None, end = None,
-    coupons = False
-    ):
-    prefix_sql = f'SELECT shipment_date, description, amazon_order_id, item_promotion_discount FROM `{report}.{table}`'
-    if coupons:
-        negative_filter.remove('VPC-')
-        negative_filter.remove('DCMS')
-        negative_filter.remove('Mellanni')
-    negative_str = f'''WHERE NOT REGEXP_CONTAINS({column},"{'|'.join(negative_filter)}")'''
+    negative_filter = bad_chars+ld_promos
+    negative_filter = [x.replace('%','') for x in negative_filter]
 
-    query = f'{prefix_sql} {negative_str}'
-    if code_list:
-        code_str = f''' AND REGEXP_CONTAINS({column},"{'|'.join(code_list)}")'''
-        query += code_str
-    if all([start is not None, end is not None]):
-        start, end = pd.to_datetime(start).date(), pd.to_datetime(end).date()
-        date_str = f' AND (shipment_date >= "{start}" AND shipment_date <= "{end}")'
-        query += date_str
+    @st.cache_data(show_spinner=False)
+    def read_promos(
+        report = 'auxillary_development',
+        table = 'promotions',
+        column = 'description',
+        code_list = None,
+        start = None, end = None,
+        coupons = False
+        ):
+        prefix_sql = f'SELECT shipment_date, description, amazon_order_id, item_promotion_discount FROM `{report}.{table}`'
+        if coupons:
+            negative_filter.remove('VPC-')
+            negative_filter.remove('DCMS')
+            negative_filter.remove('Mellanni')
+        negative_str = f'''WHERE NOT REGEXP_CONTAINS({column},"{'|'.join(negative_filter)}")'''
 
-    client = gc.gcloud_connect()
-    query_job = client.query(query)  # Make an API request.
-    data = query_job.result().to_dataframe()
-    client.close()
-    return data
+        query = f'{prefix_sql} {negative_str}'
+        if code_list:
+            code_str = f''' AND REGEXP_CONTAINS({column},"{'|'.join(code_list)}")'''
+            query += code_str
+        if all([start is not None, end is not None]):
+            start, end = pd.to_datetime(start).date(), pd.to_datetime(end).date()
+            date_str = f' AND (shipment_date >= "{start}" AND shipment_date <= "{end}")'
+            query += date_str
 
-@st.cache_data(show_spinner=False)
-def read_all_orders(
-    order_list,
-    report = 'auxillary_development',
-    table = 'all_order_report',
-    skus = False
-    ) -> pd.DataFrame:
-    '''
-    Read rows from all_order report from cloud targeting order from "order_list" - 
-    - pre-selected orders from promo report
-
-    Parameters
-    ----------
-    db_path : STR
-        path to all_orders.db file.
-    order_list : list
-        List of orders to select from all_orders.db file.
-
-    Returns
-    -------
-    orders : pd.DataFrame
-        selected rows from all_orders.db file limited to pre-defined columns.
-    '''
-    column_list = ['amazon_order_id','purchase_date','order_status','fulfillment_channel',
-    'sku','asin','quantity','currency','item_price','item_promotion_discount',
-    'promotion_ids','is_business_order','buyer_company_name']
-    order_str = '","'.join(order_list)
-    column_str = ', '.join(column_list)
-    if skus:
-        chunk_size = 100_000
-        chunks = len(order_list)//chunk_size+1
-        order_chunks = []
-        for c in range(chunks):
-            chunk = order_list[c*chunk_size:(c+1)*chunk_size]
-            order_chunks.append(chunk)
-        df = pd.DataFrame()
-
-        query = f'''SELECT
-                        asin,
-                        SUM(CAST(quantity AS INT)) as quantity,
-                        SUM(CAST(item_price AS FLOAT64)) as sales,
-                        SUM(CAST(item_promotion_discount AS FLOAT64)) as discount
-                    FROM `{report}.{table}`
-                    WHERE amazon_order_id IN UNNEST (@orders)
-                    GROUP BY asin'''
-                    # ORDER BY sales
-                    # DESC
-                    # '''
         client = gc.gcloud_connect()
-        for chunk in order_chunks:
+        query_job = client.query(query)  # Make an API request.
+        data = query_job.result().to_dataframe()
+        client.close()
+        return data
+
+    @st.cache_data(show_spinner=False)
+    def read_all_orders(
+        order_list,
+        report = 'auxillary_development',
+        table = 'all_order_report',
+        skus = False
+        ) -> pd.DataFrame:
+        '''
+        Read rows from all_order report from cloud targeting order from "order_list" - 
+        - pre-selected orders from promo report
+
+        Parameters
+        ----------
+        db_path : STR
+            path to all_orders.db file.
+        order_list : list
+            List of orders to select from all_orders.db file.
+
+        Returns
+        -------
+        orders : pd.DataFrame
+            selected rows from all_orders.db file limited to pre-defined columns.
+        '''
+        column_list = ['amazon_order_id','purchase_date','order_status','fulfillment_channel',
+        'sku','asin','quantity','currency','item_price','item_promotion_discount',
+        'promotion_ids','is_business_order','buyer_company_name']
+        order_str = '","'.join(order_list)
+        column_str = ', '.join(column_list)
+        if skus:
+            chunk_size = 100_000
+            chunks = len(order_list)//chunk_size+1
+            order_chunks = []
+            for c in range(chunks):
+                chunk = order_list[c*chunk_size:(c+1)*chunk_size]
+                order_chunks.append(chunk)
+            df = pd.DataFrame()
+
+            query = f'''SELECT
+                            asin,
+                            SUM(CAST(quantity AS INT)) as quantity,
+                            SUM(CAST(item_price AS FLOAT64)) as sales,
+                            SUM(CAST(item_promotion_discount AS FLOAT64)) as discount
+                        FROM `{report}.{table}`
+                        WHERE amazon_order_id IN UNNEST (@orders)
+                        GROUP BY asin'''
+                        # ORDER BY sales
+                        # DESC
+                        # '''
+            client = gc.gcloud_connect()
+            for chunk in order_chunks:
+                job_config = bigquery.QueryJobConfig(
+                    query_parameters=[
+                        bigquery.ArrayQueryParameter("orders", "STRING", chunk),
+                    ]
+                )
+
+                query_job = client.query(query, job_config=job_config)  # Make an API request.
+                orders = query_job.result().to_dataframe()
+                df = pd.concat([df, orders])
+            client.close()
+            df = df.pivot_table(values = ['quantity', 'sales', 'discount'], index = 'asin',aggfunc = 'sum')
+            
+        else:
+            query = f'''SELECT {column_str} FROM `{report}.{table}` WHERE amazon_order_id IN UNNEST (@orders)'''
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
-                    bigquery.ArrayQueryParameter("orders", "STRING", chunk),
+                    bigquery.ArrayQueryParameter("orders", "STRING", order_list),
                 ]
             )
-
+            client = gc.gcloud_connect()
             query_job = client.query(query, job_config=job_config)  # Make an API request.
             orders = query_job.result().to_dataframe()
-            df = pd.concat([df, orders])
-        client.close()
-        df = df.pivot_table(values = ['quantity', 'sales', 'discount'], index = 'asin',aggfunc = 'sum')
-        
-    else:
-        query = f'''SELECT {column_str} FROM `{report}.{table}` WHERE amazon_order_id IN UNNEST (@orders)'''
-        job_config = bigquery.QueryJobConfig(
-            query_parameters=[
-                bigquery.ArrayQueryParameter("orders", "STRING", order_list),
-            ]
-        )
-        client = gc.gcloud_connect()
-        query_job = client.query(query, job_config=job_config)  # Make an API request.
-        orders = query_job.result().to_dataframe()
-        client.close()
-    if skus:
-        df.rename(columns = {'sales':'Sales, $', 'discount':'Discount, $'}, inplace = True)
-        return df
-    orders['item_price'] = orders['item_price'].astype('float')
-    orders['item_promotion_discount'] = orders['item_promotion_discount'].astype('float')
-    orders['quantity'] = orders['quantity'].astype('int')
-    orders_pivot = orders.pivot_table(
-        values = ['item_price','quantity'],
-        index = 'amazon_order_id',
-        aggfunc = 'sum'
-        )#.reset_index()
-    return orders_pivot
+            client.close()
+        if skus:
+            df.rename(columns = {'sales':'Sales, $', 'discount':'Discount, $'}, inplace = True)
+            return df
+        orders['item_price'] = orders['item_price'].astype('float')
+        orders['item_promotion_discount'] = orders['item_promotion_discount'].astype('float')
+        orders['quantity'] = orders['quantity'].astype('int')
+        orders_pivot = orders.pivot_table(
+            values = ['item_price','quantity'],
+            index = 'amazon_order_id',
+            aggfunc = 'sum'
+            )#.reset_index()
+        return orders_pivot
 
-@st.cache_data(show_spinner=False)
-def process_data(code_list = None, start = None, end = None, coupons = False, skus = False):
-    def sort_and_split(x):
-        x = [i for i in x if i != ' ']
-        x = set(x)
-        x = sorted(x)
-        x = ' | '.join(x)
-        return x
-    if all([start is not None, end is not None]):
-        spinner_str = f'Pulling promo report for {(end-start).days} days'
-    else:
-        spinner_str = 'Pulling full promo report'
-    with st.spinner(spinner_str):
-        promos = read_promos(code_list=code_list, start = start, end = end, coupons = coupons)
-        code_pattern = '\(([A-Za-z0-9\s]{8,13})\-{0,1}\d{0,1}\)'
-        promos['promo_code'] = promos['description'].str.extract(code_pattern).fillna(' ')
-        if code_list != None:
-            promos = promos[promos['promo_code'].isin(code_list)]
-
-        promos_pivot = promos.pivot_table(
-            values = ['item_promotion_discount','description','promo_code'],
-            index = ['amazon_order_id'],
-            aggfunc = {'item_promotion_discount':'sum',
-                    'description':sort_and_split,
-                    'promo_code':sort_and_split}
-            )
-        order_list = promos_pivot.index.tolist()
-        if len(order_list) == 0:
-            total = 'Sorry, no data for this time period yet'
+    @st.cache_data(show_spinner=False)
+    def process_data(code_list = None, start = None, end = None, coupons = False, skus = False):
+        def sort_and_split(x):
+            x = [i for i in x if i != ' ']
+            x = set(x)
+            x = sorted(x)
+            x = ' | '.join(x)
+            return x
+        if all([start is not None, end is not None]):
+            spinner_str = f'Pulling promo report for {(end-start).days} days'
         else:
-            with st.spinner(f'Pulling {len(order_list)} orders'):
-                orders_pivot = read_all_orders(order_list, skus = skus)
+            spinner_str = 'Pulling full promo report'
+        with st.spinner(spinner_str):
+            promos = read_promos(code_list=code_list, start = start, end = end, coupons = coupons)
+            code_pattern = '\(([A-Za-z0-9\s]{8,13})\-{0,1}\d{0,1}\)'
+            promos['promo_code'] = promos['description'].str.extract(code_pattern).fillna(' ')
+            if code_list != None:
+                promos = promos[promos['promo_code'].isin(code_list)]
 
-                if skus:
-                    orders_pivot['Net proceeds'] = orders_pivot['Sales, $'] - orders_pivot['Discount, $']
-                    orders_pivot['Discount, %'] = round(orders_pivot['Discount, $'] / orders_pivot['Sales, $'] * 100, 1)
-                    return orders_pivot
-                
-                total = pd.merge(orders_pivot, promos_pivot, left_index = True, right_index = True)
-                total = total.pivot_table(
-                    values = ['item_price', 'quantity','item_promotion_discount'],
-                    index = ['promo_code','description'],
-                    aggfunc = 'sum').reset_index()
-                
-                total[['item_price','item_promotion_discount']] = round(total[['item_price','item_promotion_discount']] ,2)
+            promos_pivot = promos.pivot_table(
+                values = ['item_promotion_discount','description','promo_code'],
+                index = ['amazon_order_id'],
+                aggfunc = {'item_promotion_discount':'sum',
+                        'description':sort_and_split,
+                        'promo_code':sort_and_split}
+                )
+            order_list = promos_pivot.index.tolist()
+            if len(order_list) == 0:
+                total = 'Sorry, no data for this time period yet'
+            else:
+                with st.spinner(f'Pulling {len(order_list)} orders'):
+                    orders_pivot = read_all_orders(order_list, skus = skus)
 
-                total.rename(columns = {'item_price':'Sales, $', 'item_promotion_discount':'Discount, $'}, inplace = True)
-                total['Net proceeds'] = total['Sales, $'] - total['Discount, $']
-                total['Discount, %'] = round(total['Discount, $'] / total['Sales, $'] * 100, 1)
-                total = total.sort_values('quantity', ascending = False)
+                    if skus:
+                        orders_pivot['Net proceeds'] = orders_pivot['Sales, $'] - orders_pivot['Discount, $']
+                        orders_pivot['Discount, %'] = round(orders_pivot['Discount, $'] / orders_pivot['Sales, $'] * 100, 1)
+                        return orders_pivot
+                    
+                    total = pd.merge(orders_pivot, promos_pivot, left_index = True, right_index = True)
+                    total = total.pivot_table(
+                        values = ['item_price', 'quantity','item_promotion_discount'],
+                        index = ['promo_code','description'],
+                        aggfunc = 'sum').reset_index()
+                    
+                    total[['item_price','item_promotion_discount']] = round(total[['item_price','item_promotion_discount']] ,2)
 
-    return total
+                    total.rename(columns = {'item_price':'Sales, $', 'item_promotion_discount':'Discount, $'}, inplace = True)
+                    total['Net proceeds'] = total['Sales, $'] - total['Discount, $']
+                    total['Discount, %'] = round(total['Discount, $'] / total['Sales, $'] * 100, 1)
+                    total = total.sort_values('quantity', ascending = False)
 
-def prepare_for_export(df):
-    output = BytesIO()
-    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        df.to_excel(writer, sheet_name = 'Promos', index = False)
-        ff.format_header(df,writer,'Promos')
-    return output.getvalue()
+        return total
 
-@st.cache_data(show_spinner=False, suppress_st_warning = True)
-def main_promo_stats():
+    def prepare_for_export(df):
+        output = BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df.to_excel(writer, sheet_name = 'Promos', index = False)
+            ff.format_header(df,writer,'Promos')
+        return output.getvalue()
+
     end = pd.to_datetime('today')
     start = end - pd.to_timedelta(180, 'days')
 
@@ -265,6 +265,4 @@ def main_promo_stats():
             metric2.metric('Discount, $ and %', "${:,}".format(discount))
             metric2.metric('Discount, $ and %', "{:.1%}".format(percentage), label_visibility='hidden')
 
-if __name__ == '__main__':
-    if st.session_state['login']:
-        main_promo_stats()
+
