a7d1706c82c3fa4c29fb4f9a4c2fbf925f67bf6c
MISUNDERSTOOD <2djohar@gmail.com>
2023-03-30 15:27:05 +0300

    revised keyword grouping for KW processor tool

--- pages/03_Keyword Processor.py
+++ pages/03_Keyword Processor.py
@@ -34,6 +34,78 @@ if st.session_state['login']:
         #    'Position (Rank)', 'Relative Rank', 'Competitor Rank (avg)',
         #    'Ranking Competitors (count)', 'Competitor Performance Score']
 
+    def text_processing(file, cosine = True):
+        from sklearn.feature_extraction.text import TfidfVectorizer
+        from sklearn.cluster import MiniBatchKMeans, KMeans, MeanShift, AgglomerativeClustering, DBSCAN
+        from sklearn.metrics.pairwise import cosine_similarity
+        import nltk
+        # if nltk.download('all') == False:
+            # nltk.download('all')
+        from nltk.corpus import stopwords
+        from nltk.stem import WordNetLemmatizer
+        import re        
+        cluster_col = 'cluster'
+        corpus_col = 'clean kw'
+        kw_col = [x for x in ['Search Query', 'Keyword Phrase'] if x in file.columns][0]
+        def lemmatize(file, column):
+            kw = file[column].values.tolist()
+            lemmatizer = WordNetLemmatizer()
+            corpus = []
+            for i in range(len(kw)):
+                r = re.sub('[^a-zA-Z]', ' ', kw[i]).lower().split()
+                r = [word for word in r if word not in stopwords.words(['english','spanish'])]
+                r = [lemmatizer.lemmatize(word) for word in r]
+                r = ' '.join(r)
+                corpus.append(r)
+            file[corpus_col] = corpus
+            return file
+        
+        def measure_clusters(cosine_sim):
+            bins = range(2,30)
+            wcss = []
+            for i in bins:
+                kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+                kmeans.fit(cosine_sim)
+                wcss.append(kmeans.inertia_)    
+            optimal_clusters = [abs(wcss[i+1]-wcss[i]) for i in range(len(wcss)-1)].index(min([abs(wcss[i+1]-wcss[i]) for i in range(len(wcss)-1)]))
+            return optimal_clusters
+        
+        def clusterize_keywords(file, cosine):
+            corpus = file[corpus_col].values.tolist()
+            cv = TfidfVectorizer(stop_words=['english','spanish'],ngram_range=(1,3))
+            vectors = cv.fit_transform(corpus)
+            if cosine:
+                cosine_sim = cosine_similarity(vectors)
+            else:
+                cosine_sim = vectors
+            n_clusters = measure_clusters(cosine_sim)
+            st.write(f'Found {n_clusters} clusters')
+            model = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
+            clusters = model.fit_predict(cosine_sim)
+            return clusters
+        
+        def name_groups(file):
+            clusters = file[cluster_col].unique().tolist()
+            names = {}
+            for c in clusters:
+                kws = file[file[cluster_col] == c][kw_col].values.tolist()
+                kw_list = [w for line in [x.split() for x in kws] for w in line]
+                counts = pd.DataFrame(kw_list).value_counts()
+                limit = counts.describe(percentiles = [0.5,0.9])['90%']
+                counts = counts[counts>limit].index.tolist()
+                if len(counts) > 5:
+                    names[c] = ' '.join([x[0] for x in counts[:5]]) + ' +'
+                else:
+                    names[c] = ' '.join([x[0] for x in counts])
+                file[cluster_col] = file[cluster_col].replace(names)
+            return file
+        file = lemmatize(file, kw_col)
+        clusters = clusterize_keywords(file, cosine = cosine)
+        file[cluster_col] = clusters
+        file = name_groups(file)
+        del file['clean kw']
+        return file
+
 
     def lemmatize(file, column):
         import nltk
@@ -227,16 +299,12 @@ if st.session_state['login']:
             magnet = magnet.drop(drop_cols,axis = 1)
             file = pd.concat([file,magnet],axis = 0)
 
-        ## add colors from dictionary
-        # dictionary = pd.read_excel(d_path,usecols = ['Color','Color Map']).dropna()
-        # dictionary = dictionary.applymap(str.lower)
-        # colors = dictionary['Color'].dropna().unique().tolist()+dictionary['Color Map'].dropna().unique().tolist()
-        # color_kws = file[file['Keyword Phrase'].str.contains(('|').join(colors))]
-
         #add word counts and frequency scores
-        lemm, word_freq, vectors = lemmatize(file, 'Keyword Phrase')
-        file = pd.merge(file, lemm, how = 'left', on = 'Keyword Phrase')
-        file = clusterize(file,vectors,cols = None,num_clusters=8)
+        # lemm, word_freq, vectors = lemmatize(file, 'Keyword Phrase')
+        # file = pd.merge(file, lemm, how = 'left', on = 'Keyword Phrase')
+        # file = clusterize(file,vectors,cols = None,num_clusters=8)
+        word_freq = None
+        file = file = text_processing(file, cosine = True)
         return file, sums_db, file_ba_matched,file_ba_missed, word_freq, asins,top_kws,metrics
 
     st.title('Keyword processing tool')
@@ -255,14 +323,20 @@ if st.session_state['login']:
         df_slot.write(file)
 
     with st.sidebar:
-        st.header('Apply filters')
-        include_area,exclude_area = st.container(), st.container()
+        st.header('Apply filters (EXPERIMENTAL!)')
+        include_area,exclude_area, kw_area = st.container(), st.container(), st.container()
         include = include_area.text_input('Words to include?',key = 'include')
         include_all = include_area.radio('Include mode:',['All','Any'],horizontal = True)
         exclude = exclude_area.text_input('Words to exclude?', key = 'exclude')
         exclude_all = exclude_area.radio('Exclude mode:',['All','Any'],horizontal = True)
+        if 'kws' in st.session_state:
+            kw_group = kw_area.selectbox('Keyyword groups',['All']+st.session_state['kws'])
+            if kw_group and kw_group != 'All':
+                st.session_state['display_file'] = st.session_state['file'][st.session_state['file']['cluster'] == kw_group]
+                df_slot.write(st.session_state['file']['cluster'] == kw_group)
+            elif kw_group == 'All':
+                df_slot.write(st.session_state['display_file'])
         st.button('Clear filters', on_click= clear_filters)
-
 
     with st.expander('Upload files'):
         cerebro_file = st.file_uploader('Select Cerebro file')
@@ -311,17 +385,24 @@ if st.session_state['login']:
         else:
             magnet = ''
 
+
+
+    if 'display_file' in st.session_state:
+        df_slot.write(st.session_state['display_file'])
+        
+
     if st.button('Process keywords') and cerebro_file:
         file, sums_db, file_ba_matched,file_ba_missed, word_freq,asins,top_kws,metrics = process_file(asins,cerebro,ba,magnet,n_clusters,bins)
         st.session_state['file'] = file
         alpha_asin.bar_chart(sums_db.T[metrics[1]])
         magnet_words.text_area('Magnet keyword research', value = "\n".join(top_kws), height = 250)
-        display_file = file.drop(columns = asins)
-        st.session_state['df'] = display_file
-        df_slot.write(display_file)
+        st.session_state['display_file'] = file.drop(columns = asins)
+        st.session_state['df'] = st.session_state['display_file']
+        st.session_state['kws'] = file['cluster'].unique().tolist()
+        df_slot.write(st.session_state['display_file'])
         
-        output = BytesIO()
-        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        st.session_state['output'] = BytesIO()
+        with pd.ExcelWriter(st.session_state['output'], engine='xlsxwriter') as writer:
             file.to_excel(writer, sheet_name = 'Keywords', index = False)
             workbook = writer.book
             worksheet = writer.sheets['Keywords']
@@ -361,10 +442,13 @@ if st.session_state['login']:
                     {'type': '3_color_scale','max_color':'red','min_color':'green'})
             except:
                 pass
-            word_freq.to_excel(writer, sheet_name = 'word_frequency', index = False)
-            ff.format_header(word_freq, writer, 'word_frequency')    
-        
-        st.download_button('Download results',output.getvalue(), file_name = 'test.xlsx')
+            try:
+                word_freq.to_excel(writer, sheet_name = 'word_frequency', index = False)
+                ff.format_header(word_freq, writer, 'word_frequency')
+            except:
+                pass
+    if any(['display_file' in st.session_state, 'df' in st.session_state]):
+        st.download_button('Download results',st.session_state['output'].getvalue(), file_name = 'test.xlsx')
 
     def filter_plus(file,filters, combination):
         words = [w.lower().strip() for w in filters.split(',')]
@@ -383,12 +467,15 @@ if st.session_state['login']:
 
     if include != '' and 'df' in st.session_state:
         if 'filtered_df' in st.session_state:
-            filtered_file = st.session_state['filtered_df']
+            st.session_state['filtered_file'] = st.session_state['filtered_df']
         else:
-            filtered_file = st.session_state['df']
-        filtered_file = filter_plus(filtered_file,include, include_all)
-        st.session_state['filtered_df'] = filtered_file
-        df_slot.write(filtered_file)
+            st.session_state['filtered_file'] = st.session_state['df']
+        st.session_state['filtered_file'] = filter_plus(st.session_state['filtered_file'],include, include_all)
+        if len(st.session_state['filtered_file'] == 0):
+            df_slot.write('Nothing matched')
+        else:
+            st.session_state['filtered_df'] = st.session_state['filtered_file']
+            df_slot.write(st.session_state['filtered_file'])
 
     if exclude != '' and 'df' in st.session_state:
         if 'filtered_df' in st.session_state:
@@ -396,8 +483,11 @@ if st.session_state['login']:
         else:
             filtered_file = st.session_state['df']
         filtered_file = filter_minus(filtered_file,exclude, exclude_all)
-        st.session_state['filtered_df'] = filtered_file
-        df_slot.write(filtered_file)
+        if len(filtered_file == 0):
+            df_slot.write('Nothing matched')
+        else:
+            st.session_state['filtered_df'] = filtered_file
+            df_slot.write(filtered_file)
 
 
 # date1,date2 = st.slider(
