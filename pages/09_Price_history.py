d41bc4dc934e3d7ce3dc6ebcd920bf22dbdf3011
MISUNDERSTOOD <2djohar@gmail.com>
2023-05-31 19:46:11 +0300

    price tracker minor updates

--- pages/09_Price_history.py
+++ pages/09_Price_history.py
@@ -62,7 +62,7 @@ def get_prices(queue):
         query_job = client.query(query)  # Make an API request.
         data = query_job.result().to_dataframe()
         client.close()
-        data['datetime'] = pd.to_datetime(data['datetime'], format = "%Y-%m-%d %H:%M:%S")
+        data['datetime'] = pd.to_datetime(data['datetime'], format = '%m/%d/%Y %H:%M')
         data = data.sort_values('datetime')
         queue.put(data)
         return None
