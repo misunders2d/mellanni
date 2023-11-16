e96c2ea8e70f43b178136c234d8509b40d23ca7c
MISUNDERSTOOD <2djohar@gmail.com>
2023-11-16 14:56:55 +0200

    title changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import streamlit_authenticator as stauth
-st.set_page_config(page_title = 'M Tools App', page_icon = 'media/logo.ico',layout="wide")
+st.set_page_config(page_title = 'Mellanni Tools App', page_icon = 'media/logo.ico',layout="wide")
 
 import login
 st.session_state['login'], st.session_state['name'] = login.login()
