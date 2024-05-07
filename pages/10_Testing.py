1b1006d5e9c88316fd8a388563d4fef068883f29
MISUNDERSTOOD <2djohar@gmail.com>
2024-05-07 20:42:30 +0300

    testing new kw grouping

--- pages/10_Testing.py
+++ pages/10_Testing.py
@@ -9,7 +9,8 @@ from modules import formatting as ff
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 # from sklearn.feature_extraction.text import TfidfVectorizer
 # from sklearn.cluster import KMeans
@@ -36,149 +37,224 @@ st.set_page_config(page_title = 'M Tools App', page_icon = 'media/logo.ico',layo
 import login_google
 st.session_state['login'] = login_google.login()
 
+# def text_processing(file):
+#     cluster_col = 'cluster'
+#     corpus_col = 'clean kw'
+#     kw_col = [x for x in ['Search Query', 'Keyword Phrase'] if x in file.columns][0]
+#     # def lemmatize(file, column):
+#     #     kw = file[column].values.tolist()
+#     #     lemmatizer = WordNetLemmatizer()
+#     #     corpus = []
+#     #     for i in range(len(kw)):
+#     #         r = re.sub('[^a-zA-Z]', ' ', kw[i]).lower().split()
+#     #         r = [word for word in r if word not in stopwords.words(['english','spanish'])]
+#     #         r = [lemmatizer.lemmatize(word) for word in r]
+#     #         r = ' '.join(r)
+#     #         corpus.append(r)
+#     #     file[corpus_col] = corpus
+#     #     return file
+    
+#     # def measure_clusters(vectors,bins):
+#     #     sims = {'cosine':cosine_similarity(vectors), 'nocosine':vectors.toarray()}
+#     #     optimal = 1000000000
+#     #     for key,sim in sims.items():
+#     #         wcss = []
+#     #         for i in bins:
+#     #             model = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+#     #             model.fit(sim)
+#     #             wcss.append(model.inertia_)
+#     #         minimal = min([abs(wcss[i+1]-wcss[i]) for i in range(len(wcss)-1)])
+#     #         if minimal < optimal:
+#     #             optimal = minimal
+#     #             optimal_clusters = [abs(wcss[i+1]-wcss[i]) for i in range(len(wcss)-1)].index(minimal)
+#     #             result = [key, optimal_clusters, minimal]
+#     #     st.success(f'Found {result[1]} keyword groups')
+#     #     return sims[result[0]],result[1], result[0]
+    
+#     # def clusterize_keywords(file):
+#     #     corpus = file[corpus_col].values.tolist()
+#     #     cv = TfidfVectorizer(stop_words=['english','spanish'],ngram_range=(1,3))
+#     #     vectors = cv.fit_transform(corpus)
+#     #     if len(corpus)<10:
+#     #         bins = 3
+#     #     elif len(corpus)<=50:
+#     #         bins = range(1,4)
+#     #     elif len(corpus) > 50:
+#     #         bins = range(2,30)
+#     #     sim, n_clusters, method = measure_clusters(vectors,bins)
+#     #     model = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
+#     #     clusters = model.fit_predict(sim)
+#     #     return clusters
+#     # def name_groups(file):
+#     #     clusters = file[cluster_col].unique().tolist()
+#     #     # names = {}
+#     #     # for c in clusters:
+#     #     #     kws = file[file[cluster_col] == c][kw_col].values.tolist()
+#     #     #     kw_list = [w for line in [x.split() for x in kws] for w in line]
+#     #     #     counts = pd.DataFrame(kw_list).value_counts()
+#     #     #     limit = counts.describe(percentiles = [0.5,0.9])['90%']
+#     #     #     counts = counts[counts>=limit].index.tolist()
+#     #     #     if len(counts) > 5:
+#     #     #         names[c] = ' '.join([x[0] for x in counts[:5]]) + ' +'
+#     #     #     else:
+#     #     #         names[c] = ' '.join([x[0] for x in counts])
+#     #     #     file[cluster_col] = file[cluster_col].replace(names)
+#     #     #     file[cluster_col] = file[cluster_col].replace('','other')
+
+#     #     names = {}
+#     #     for c in clusters:
+#     #         group = file.loc[file[cluster_col] == c][corpus_col]
+#     #         vectorizer = TfidfVectorizer()
+#     #         X = vectorizer.fit_transform(group)
+#     #         names[c] = ' '.join(pd.DataFrame(
+#     #             X.toarray(), columns=vectorizer.get_feature_names_out()
+#     #             ).sum().sort_values(ascending = False)[:3].index.tolist())
+#     #         file[cluster_col] = file[cluster_col].replace(names)
+#     #         file[cluster_col] = file[cluster_col].replace('','other')
+
+
+#     #     return file
+#     # file = lemmatize(file, kw_col)
+#     # clusters = clusterize_keywords(file)
+#     # file[cluster_col] = clusters
+#     # file = name_groups(file)
+    
+#     def group_keywords(keywords, min_df=1, max_df=100):
+#         """
+#         Groups a list of product keywords into buckets based on semantic similarity using the Elbow Method to determine optimal cluster count.
+#         Also assigns a name to each cluster based on the most important keyword.
+
+#         Args:
+#             keywords: List of product keywords.
+#             min_df: Minimum document frequency for a word to be considered in vocabulary (default: 2).
+#             max_df: Maximum document frequency for a word to be considered in vocabulary (default: 0.5).
+
+#         Returns:
+#             A dictionary where keys are cluster names and values are lists of keywords belonging to that cluster.
+#         """
+#         vectorizer = TfidfVectorizer(min_df=min_df, max_df=max_df)
+#         tfidf_matrix = vectorizer.fit_transform(keywords)
+
+#         # Evaluate a range of clusters and store inertia values
+#         inertias = []
+#         for num_clusters in range(2, 30):  # Adjust range based on expected number of clusters
+#             kmeans = KMeans(n_clusters=num_clusters)
+#             kmeans.fit(tfidf_matrix)
+#             inertias.append(kmeans.inertia_)
+
+#         # Identify the elbow point using silhouette score
+#         silhouette_scores = []
+#         for num_clusters in range(2, 30):
+#             kmeans = KMeans(n_clusters=num_clusters)
+#             kmeans.fit(tfidf_matrix)
+#             silhouette_scores.append(silhouette_score(tfidf_matrix, kmeans.labels_))
+#         best_cluster_num = silhouette_scores.index(max(silhouette_scores)) + 2
+
+#         # Perform clustering with the optimal number of clusters
+#         kmeans = KMeans(n_clusters=best_cluster_num)
+#         kmeans.fit(tfidf_matrix)
+
+#         clusters = {}
+#         for i, keyword in enumerate(keywords):
+#             cluster_num = kmeans.labels_[i]
+#             if cluster_num not in clusters:
+#                 clusters[cluster_num] = []
+#             clusters[cluster_num].append(keyword)
+
+#         # Assign cluster names based on most important keyword
+#         cluster_names = {}
+#         for cluster_num, keywords in clusters.items():
+#             tfidf_features = dict(zip(vectorizer.get_feature_names_out(), kmeans.cluster_centers_[cluster_num]))
+#             # Assuming keywords are single words, get the word with highest TF-IDF score
+#             cluster_names[cluster_num] = max(tfidf_features, key=tfidf_features.get)
+
+#         # Combine cluster names and keywords
+#         named_clusters = {cluster_names[key]: clusters[key] for key in clusters.keys()}
+#         return named_clusters
+    
+#     keywords = file[kw_col].unique().tolist()
+#     clusters = group_keywords(keywords)
+
+#     for cluster in clusters:
+#         file.loc[file[kw_col].isin(clusters[cluster]), cluster_col] = cluster
+
+#     del file['clean kw']
+#     return file
+
 def text_processing(file):
     cluster_col = 'cluster'
     corpus_col = 'clean kw'
     kw_col = [x for x in ['Search Query', 'Keyword Phrase'] if x in file.columns][0]
-    # def lemmatize(file, column):
-    #     kw = file[column].values.tolist()
-    #     lemmatizer = WordNetLemmatizer()
-    #     corpus = []
-    #     for i in range(len(kw)):
-    #         r = re.sub('[^a-zA-Z]', ' ', kw[i]).lower().split()
-    #         r = [word for word in r if word not in stopwords.words(['english','spanish'])]
-    #         r = [lemmatizer.lemmatize(word) for word in r]
-    #         r = ' '.join(r)
-    #         corpus.append(r)
-    #     file[corpus_col] = corpus
-    #     return file
     
-    # def measure_clusters(vectors,bins):
-    #     sims = {'cosine':cosine_similarity(vectors), 'nocosine':vectors.toarray()}
-    #     optimal = 1000000000
-    #     for key,sim in sims.items():
-    #         wcss = []
-    #         for i in bins:
-    #             model = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    #             model.fit(sim)
-    #             wcss.append(model.inertia_)
-    #         minimal = min([abs(wcss[i+1]-wcss[i]) for i in range(len(wcss)-1)])
-    #         if minimal < optimal:
-    #             optimal = minimal
-    #             optimal_clusters = [abs(wcss[i+1]-wcss[i]) for i in range(len(wcss)-1)].index(minimal)
-    #             result = [key, optimal_clusters, minimal]
-    #     st.success(f'Found {result[1]} keyword groups')
-    #     return sims[result[0]],result[1], result[0]
-    
-    # def clusterize_keywords(file):
-    #     corpus = file[corpus_col].values.tolist()
-    #     cv = TfidfVectorizer(stop_words=['english','spanish'],ngram_range=(1,3))
-    #     vectors = cv.fit_transform(corpus)
-    #     if len(corpus)<10:
-    #         bins = 3
-    #     elif len(corpus)<=50:
-    #         bins = range(1,4)
-    #     elif len(corpus) > 50:
-    #         bins = range(2,30)
-    #     sim, n_clusters, method = measure_clusters(vectors,bins)
-    #     model = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    #     clusters = model.fit_predict(sim)
-    #     return clusters
-    # def name_groups(file):
-    #     clusters = file[cluster_col].unique().tolist()
-    #     # names = {}
-    #     # for c in clusters:
-    #     #     kws = file[file[cluster_col] == c][kw_col].values.tolist()
-    #     #     kw_list = [w for line in [x.split() for x in kws] for w in line]
-    #     #     counts = pd.DataFrame(kw_list).value_counts()
-    #     #     limit = counts.describe(percentiles = [0.5,0.9])['90%']
-    #     #     counts = counts[counts>=limit].index.tolist()
-    #     #     if len(counts) > 5:
-    #     #         names[c] = ' '.join([x[0] for x in counts[:5]]) + ' +'
-    #     #     else:
-    #     #         names[c] = ' '.join([x[0] for x in counts])
-    #     #     file[cluster_col] = file[cluster_col].replace(names)
-    #     #     file[cluster_col] = file[cluster_col].replace('','other')
-
-    #     names = {}
-    #     for c in clusters:
-    #         group = file.loc[file[cluster_col] == c][corpus_col]
-    #         vectorizer = TfidfVectorizer()
-    #         X = vectorizer.fit_transform(group)
-    #         names[c] = ' '.join(pd.DataFrame(
-    #             X.toarray(), columns=vectorizer.get_feature_names_out()
-    #             ).sum().sort_values(ascending = False)[:3].index.tolist())
-    #         file[cluster_col] = file[cluster_col].replace(names)
-    #         file[cluster_col] = file[cluster_col].replace('','other')
-
-
-    #     return file
-    # file = lemmatize(file, kw_col)
-    # clusters = clusterize_keywords(file)
-    # file[cluster_col] = clusters
-    # file = name_groups(file)
-    
-    def group_keywords(keywords, min_df=1, max_df=100):
+    def group_keywords(keywords_list, max_clusters = 30):
         """
-        Groups a list of product keywords into buckets based on semantic similarity using the Elbow Method to determine optimal cluster count.
-        Also assigns a name to each cluster based on the most important keyword.
-
+        Groups a list of product keywords into clusters based on semantic similarity.
+        
         Args:
-            keywords: List of product keywords.
-            min_df: Minimum document frequency for a word to be considered in vocabulary (default: 2).
-            max_df: Maximum document frequency for a word to be considered in vocabulary (default: 0.5).
-
+            keywords: A list of strings representing product keywords.
+        
         Returns:
             A dictionary where keys are cluster names and values are lists of keywords belonging to that cluster.
         """
-        vectorizer = TfidfVectorizer(min_df=min_df, max_df=max_df)
-        tfidf_matrix = vectorizer.fit_transform(keywords)
-
-        # Evaluate a range of clusters and store inertia values
-        inertias = []
-        for num_clusters in range(2, 30):  # Adjust range based on expected number of clusters
-            kmeans = KMeans(n_clusters=num_clusters)
+        stop_words = stopwords.words('english')
+        lemmatizer = WordNetLemmatizer()
+        
+        # Preprocess text data
+        processed_keywords = []
+        for keyword in keywords_list:
+            # lowercase, remove stopwords, lemmatize
+            words = [lemmatizer.lemmatize(w.lower()) for w in keyword.split() if w not in stop_words]
+            processed_keywords.append(" ".join(words))
+        
+        file[corpus_col] = processed_keywords
+        
+        # Vectorize the keywords using TF-IDF
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(processed_keywords)
+        
+        # Use the Elbow method to find the optimal number of clusters
+        wcss = []
+        for k in range(1, max_clusters):
+            kmeans = KMeans(n_clusters=k, random_state=0)
             kmeans.fit(tfidf_matrix)
-            inertias.append(kmeans.inertia_)
-
-        # Identify the elbow point using silhouette score
-        silhouette_scores = []
-        for num_clusters in range(2, 30):
-            kmeans = KMeans(n_clusters=num_clusters)
-            kmeans.fit(tfidf_matrix)
-            silhouette_scores.append(silhouette_score(tfidf_matrix, kmeans.labels_))
-        best_cluster_num = silhouette_scores.index(max(silhouette_scores)) + 2
-
-        # Perform clustering with the optimal number of clusters
-        kmeans = KMeans(n_clusters=best_cluster_num)
+            wcss.append(kmeans.inertia_)
+        
+        optimal_k = min(range(1, max_clusters-1), key=lambda i: wcss[i-1] - wcss[i])
+        
+        # KMeans clustering with the optimal number of clusters
+        kmeans = KMeans(n_clusters=optimal_k, random_state=0)
         kmeans.fit(tfidf_matrix)
-
-        clusters = {}
-        for i, keyword in enumerate(keywords):
-            cluster_num = kmeans.labels_[i]
-            if cluster_num not in clusters:
-                clusters[cluster_num] = []
-            clusters[cluster_num].append(keyword)
-
-        # Assign cluster names based on most important keyword
+        cluster_labels = kmeans.labels_
+        
+        clustered_keywords = {}
+        for i, keyword in enumerate(processed_keywords):
+            cluster = cluster_labels[i]
+            if cluster not in clustered_keywords:
+                clustered_keywords[cluster] = []
+            clustered_keywords[cluster].append(keyword)
+            
+    
         cluster_names = {}
-        for cluster_num, keywords in clusters.items():
+        for cluster_num, keywords in clustered_keywords.items():
             tfidf_features = dict(zip(vectorizer.get_feature_names_out(), kmeans.cluster_centers_[cluster_num]))
             # Assuming keywords are single words, get the word with highest TF-IDF score
             cluster_names[cluster_num] = max(tfidf_features, key=tfidf_features.get)
-
-        # Combine cluster names and keywords
-        named_clusters = {cluster_names[key]: clusters[key] for key in clusters.keys()}
+    
+        named_clusters = {cluster_names[key]: clustered_keywords[key] for key in cluster_names.keys()}
+    
         return named_clusters
     
-    keywords = file[kw_col].unique().tolist()
-    clusters = group_keywords(keywords)
+    keywords_list = file[kw_col].unique().tolist()
+    clusters = group_keywords(keywords_list)
+    file[cluster_col] = 'other'
 
-    for cluster in clusters:
-        file.loc[file[kw_col].isin(clusters[cluster]), cluster_col] = cluster
+    for cluster in clusters.keys():
+        file.loc[file[corpus_col].isin(clusters[cluster]), cluster_col] = cluster
 
-    del file['clean kw']
+    del file[corpus_col]
     return file
+
+
 
 def sqp_analyze(file):
     # bins = [0.4, 0.7]
